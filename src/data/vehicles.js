cdab388f117106ad3fa978c820ab543d04809b1d
RizkiRamadan7777 <phonebanana314@gmail.com>
2025-04-26 11:51:00 +0700

    Create vehicles.js

---
+++ src/data/vehicles.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    id: 1,
+    slug: 'xpander',
+    name: 'Mitsubishi Xpander',
+    type: 'MPV 7-Seater',
+    price: 'Rp 310.000.000',
+    image: '/images/xpander.jpg',
+    transmission: 'Automatic',
+    engine: '1.5L MIVEC',
+    fuel: 'Pertalite',
+    isBestSeller: true,
+    features: [
+      'LED Headlights',
+      '8-inch Touchscreen',
+      'Dual SRS Airbags',
+      'Rear Parking Sensors'
+    ]
+  },
+  // Tambahkan model lainnya
+]
